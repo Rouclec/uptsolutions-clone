0fb378c0763d8702ce77746c064dea71d802891b
Rouclec <senatorasonganyi97@gmail.com>
2023-09-13 10:14:04 +0100

    local changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,17 @@
-import { Header, OrderAlert, SideBar, Stats } from "@/components";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import {
-  HiOutlineChevronDown,
-  HiOutlineMagnifyingGlass,
-  HiOutlinePlus,
-  HiOutlineTrash,
-} from "react-icons/hi2";
-import { HiOutlineCloudDownload } from "react-icons/hi";
-import Image from "next/image";
-import Link from "next/link";
-import { roboto, roboto_slab } from "./_app";
+import { SideBar } from "@/components";
 import PrintSummary from "@/components/dashboard/PrintSummary";
 import OrderSummary from "@/components/dashboard/OrderSummary";
 import { getSession, useSession } from "next-auth/react";
 import { User } from "@/types";
 
 export default function Home() {
-  const [isAdmin, setIsAdmin] = useState(false);
   const session = useSession();
-  const [newOrder, setNewOrder] = useState(1);
-  const [showAlert, setShowAlert] = useState(true); 
   const user = session?.data?.user as User;
-  const router = useRouter();
-
 
   return (
     <SideBar>
       <div className="">
-        {(user?.role?.code != "admin" ? <PrintSummary /> : <OrderSummary />)}
+        {user?.role?.code != "admin" ? <PrintSummary /> : <OrderSummary />}
       </div>
     </SideBar>
   );
